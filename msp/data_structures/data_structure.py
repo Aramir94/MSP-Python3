524307a5fb9ae9a4e8a71a0dde10c9f947e39247
LukeARohl <Luke.A.Rohl@gmail.com>
2020-09-16 03:02:40 -0500

    remove __future__

--- msp/data_structures/data_structure.py
+++ msp/data_structures/data_structure.py
@@ -1,4 +1,3 @@
-from __future__ import annotations
 from abc import ABC, abstractmethod
 from typing import List
 
@@ -6,7 +5,7 @@ from typing import List
 class DataStructure(ABC):
     @staticmethod
     @abstractmethod
-    def parse(data: List[int]) -> DataStructure:
+    def parse(data: List[int]):
         """
         Creates a `DataStructure <msp.data_structures.data_structure.html>`_ object from the given data
         :param data:
