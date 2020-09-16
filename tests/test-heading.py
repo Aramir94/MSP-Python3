f9e5f329485300f5e23531e2b6d974dea738b01d
LukeARohl <Luke.A.Rohl@gmail.com>
2020-09-16 03:12:45 -0500

    test-heading initial commit

---
+++ tests/test-heading.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+"""test-altitude.py: Test script to send RC commands to a MultiWii Board."""
+
+import sys
+
+from msp.multiwii import MultiWii
+
+if __name__ == "__main__":
+    print_debug = sys.argv[1].lower() == 'true'
+    fc = MultiWii("/dev/ttyS0", print_debug)
+    fc.start()  # Start HIL Thread
+    try:
+        while True:
+            print(fc.get_heading())
+
+    except Exception as err:
+        print("Error on Main: " + str(err))
